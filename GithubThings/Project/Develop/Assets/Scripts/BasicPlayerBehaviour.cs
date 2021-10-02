e931ad6590286ec53c0d6d32af873348cc5e176c
MrAlridge <rnloser@163.com>
2021-10-02 12:03:23 +0800

    增加了一些东西，却没办法解决触地检测

--- GithubThings/Project/Develop/Assets/Scripts/BasicPlayerBehaviour.cs
+++ GithubThings/Project/Develop/Assets/Scripts/BasicPlayerBehaviour.cs
@@ -7,16 +7,19 @@ public class BasicPlayerBehaviour : MonoBehaviour
     // -----Component Varieties-----
     public Collider2D playerCollider;
     public Rigidbody2D playerRigidbody;
+    public Animator playerAnimator;
+    public SpriteRenderer playerRender;
     // -----State Data-----
-    private bool isGround;                      // 是否着地
+    public bool isGround;                      // 是否着地
     private bool isMoveable = true;             // 是否可动
     public float playerSpeed;                   // 初始移动速度
     public float playerJumpForce;               // 初始跳跃力度
-    private float playerJumpCount;              // 玩家已跳跃次数
+    public float playerJumpCount;              // 玩家已跳跃次数
     // -----Input Data-----
     private float horizontalInputValue;         // 水平轴输入
     private float verticalInputValue;           // 垂直轴输入
     private bool jumpInputState;                // 跳跃键输入
+    // -----Other Thing-----
 
     void Start()
     {
@@ -25,6 +28,8 @@ public class BasicPlayerBehaviour : MonoBehaviour
 
     void Update()
     {
+        GroundScan();
+        UpdateRender();
         GetInput();
         if(isMoveable)                          // 移动操作逻辑
         {
@@ -40,18 +45,39 @@ public class BasicPlayerBehaviour : MonoBehaviour
                 }
             }
         }
+        
     }
 
     void Jump()                                 // 跳跃函数
     {
         playerRigidbody.AddForce(new Vector2(0, playerJumpForce));
+        playerJumpCount++;
     }
 
     void GetInput()                             // 输入数据更新
     {
         horizontalInputValue = Input.GetAxis("Horizontal");
         verticalInputValue = Input.GetAxis("Vertical");
-        jumpInputState = Input.GetButtonDown("Jump");
+        jumpInputState = Input.GetKeyDown(KeyCode.Space);
+    }
+
+    void UpdateRender()
+    {
+        if(playerRigidbody.velocity.x > 0)
+        {
+            if(playerRender.flipX != false)
+            {
+                playerRender.flipX = false;
+            }
+        }else{
+            if(playerRigidbody.velocity.x < 0)
+            {
+                if(playerRender.flipX != true)
+                {
+                    playerRender.flipX = true;
+                }
+            }
+        }
     }
 
     void SetIsGround(bool setBool)              // 封装变值方法
@@ -60,4 +86,22 @@ public class BasicPlayerBehaviour : MonoBehaviour
             isGround = setBool;
     }
 
+    void GroundScan()                           // 触地检测
+    {
+        bool groundDetect = false;
+        Collider2D[] contacts = new Collider2D[2];
+        playerCollider.GetContacts(contacts);
+        foreach (Collider2D item in contacts)
+        {
+            if (item.gameObject.tag == "Tile")
+            {
+                groundDetect = true;
+            }
+        }
+        if(groundDetect)
+        {
+            SetIsGround(true);
+            playerJumpCount = 0;
+        }
+    }
 }
